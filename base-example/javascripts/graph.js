29035cc977c37e27ea182c0ab0b7a79e17be95b2
jiefenn8 <jiefen8nz@gmail.com>
2019-02-18 23:52:28 +1300

    Improvements to code
    - Removed redundant code
    - Optimized code to use configurable variables

--- base-example/javascripts/graph.js
+++ base-example/javascripts/graph.js
@@ -11,13 +11,30 @@ var linkStrength = 2;
 
 //Requires on existing element #graph and dependent on its size.
 function renderGraphV3(graph) {
+  //Backward support for d3 v3 api
+  var obj = {};
+  graph.nodes.forEach(function(d, i) {
+    obj[d.id] = i; // create an object to look up a node's index by id
+  });
+
+  graph.links.forEach(function(d) {
+    d.source = obj[d.source]; // look up the index of source
+    d.target = obj[d.target]; // look up the index of target
+  });
+
   d3.select("svg").remove();
+
+  var element = document.getElementById("graph");
+  var rect = element.getBoundingClientRect();
+  var width = rect.width;
+  var height = (rect.width / widthRatio) * heightRatio;
+
   var svg = d3
     .select("#graph")
     .classed("svg-container", true)
     .append("svg")
     .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", "0 0 600 400")
+    .attr("viewBox", "0 0 900 507")
     .classed("svg-content-responsive", true)
     .append("g");
   /*.call(
@@ -35,16 +52,9 @@ function renderGraphV3(graph) {
     )
     .append("g");*/
 
-  var element = document.getElementById("graph");
-  var rect = element.getBoundingClientRect();
-  var width = rect.width;
-  var height = (rect.width / widthRatio) * heightRatio;
-
   //Render size - based on width and height ratio
-  var w = width,
-    h = height;
 
-  var color = d3.scale.category20(); //d3.scaleOrdinal(d3.schemeCategory20);
+  var color = d3.scale.category20();
 
   var force = d3.layout
     .force()
@@ -99,22 +109,6 @@ function renderGraphV3(graph) {
     .on("mouseout", mouseout)
     .call(force.drag);
 
-  var linkPath = link
-    .append("svg:path")
-    .attr("class", function(d) {
-      return "link " + d.value;
-    })
-    .attr("marker-end", function(d) {
-      return "url(#" + d.value + ")";
-    });
-
-  var textPath = link
-    .append("svg:path")
-    .attr("id", function(d) {
-      return d.source.index + "_" + d.target.index;
-    })
-    .attr("class", "textpath");
-
   var text = svg
     .append("svg:g")
     .selectAll("g")
@@ -122,7 +116,7 @@ function renderGraphV3(graph) {
     .enter()
     .append("svg:g");
 
-  // A copy of the text with a thick white stroke for legibility.
+  //Text shadow overlay for legibility
   text
     .append("svg:text")
     .attr("x", 8)
@@ -137,26 +131,7 @@ function renderGraphV3(graph) {
     .attr("x", 8)
     .attr("y", ".31em")
     .text(function(d) {
-      return d.name;
-    });
-
-  var path_label = svg
-    .append("svg:g")
-    .selectAll(".path_label")
-    .data(force.links())
-    .enter()
-    .append("svg:text")
-    .attr("class", "path_label")
-    .append("svg:textPath")
-    .attr("startOffset", "50%")
-    .attr("text-anchor", "middle")
-    .attr("xlink:href", function(d) {
-      return "#" + d.source.index + "_" + d.target.index;
-    })
-    .style("fill", "#000")
-    .style("font-family", "Arial")
-    .text(function(d) {
-      return d.value;
+      return d.id;
     });
 
   svg
@@ -174,16 +149,11 @@ function renderGraphV3(graph) {
   });
 
   node.append("title").text(function(d) {
-    return d.name;
+    return d.id;
   });
 
   force.on("tick", function(e) {
     var k = 6 * e.alpha;
-    if (booleanResized == true) {
-      (width = $(window).width()), (height = $(window).height());
-      force.size([width, height]).resume();
-      booleanResized = false;
-    }
 
     graph.nodes.forEach(function(o, i) {
       o.y += i & 1 ? k : -k;
@@ -214,10 +184,10 @@ function renderGraphV3(graph) {
 
     node
       .attr("cx", function(d) {
-        return (d.x = Math.max(r, Math.min(w - r, d.x)));
+        return (d.x = Math.max(r, Math.min(width - r, d.x)));
       })
       .attr("cy", function(d) {
-        return (d.y = Math.max(r, Math.min(h - r, d.y)));
+        return (d.y = Math.max(r, Math.min(height - r, d.y)));
       });
   });
 
@@ -232,7 +202,6 @@ function neighboring(a, b) {
 }
 
 function mouseover(d) {
-  console.log(r);
   //Enlarge selected and neigbouring nodes
   d3.selectAll(".node")
     .transition()
@@ -250,7 +219,7 @@ function mouseover(d) {
       return o.source === d || o.target === d ? 1 : 0.1;
     });
 
-  //clock non-neightbour nodes of selected
+  //cloak non-neightbour nodes of selected
   d3.selectAll(".node")
     .transition()
     .duration(transitionSpeed)
@@ -279,8 +248,3 @@ function mouseout() {
     .duration(transitionSpeed)
     .style("opacity", 1);
 }
-
-var booleanResized;
-window.onresize = function() {
-  booleanResized = true;
-};
